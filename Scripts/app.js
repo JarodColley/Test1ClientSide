9b752a6ed96bafc8a6f3529db29f5db50bfb2c6f
JarodColley <47607281+JarodColley@users.noreply.github.com>
2020-01-28 15:46:40 -0500

    hid and showed the paragraph when the button is clicked

--- Scripts/app.js
+++ Scripts/app.js
@@ -37,12 +37,16 @@ let app;
         //hiding the paragraph
         paragraph.style.display = "none";
 
-        //event listener for the large button
+        //event listener and handler for the large button click
         largeButton.addEventListener("click", 
         function(){
+            //if the button reads learn more and its clicked change the text to Hide details and display the paragraph
             if(largeButton.textContent == "Learn More")
-            { largeButton.textContent = "Hide Details"} 
-            else {largeButton.textContent = "Learn More"}});
+            { largeButton.textContent = "Hide Details"
+            paragraph.style.display = "block";  } 
+            //else if the button is clicked set the text to Learn more and hide the paragraph
+            else {largeButton.textContent = "Learn More"
+            paragraph.style.display = "none";}});
 
         Main();
     }
